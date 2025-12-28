f2ab8d4e4f2dcf9e641edd66724b79d272ec8f3d
Seth Taylor <128197176+DevSethT@users.noreply.github.com>
2025-12-27 20:22:50 -0600

    project cards and global style

--- Portfolio/src/componets/App/App.jsx
+++ Portfolio/src/componets/App/App.jsx
@@ -1,32 +1,28 @@
-import './App.css'
-import { useEffect, useState } from "react";
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-
-import NavBar from '../NavBar/NavBar';
-import Main from '../Main/Main';
-import Projects from '../Projects/Projects';
-import Contact from '../Contact/Contact';
-import About from '../About/About';
-import Header from '../Header/Header'
-import PageNotFound from '../PageNotFound/PageNotFound';
-
+import NavBar from "../NavBar/NavBar";
+import Main from "../Main/Main";
+import Projects from "../Projects/Projects";
+import Contact from "../Contact/Contact";
+import About from "../About/About";
+import Header from "../Header/Header";
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 function App() {
-
   return (
-    <>
-    <Header />
-    <NavBar />
+    <div className="app">
+      <Header />
+      <NavBar />
       <Routes>
-        <Route path='*' element={<PageNotFound/>} />
-        <Route path='/' element={<Main />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact-me' element={<Contact />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact-me" element={<Contact />} />
       </Routes>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Portfolio/src/componets/Projects/Projects.jsx
+++ Portfolio/src/componets/Projects/Projects.jsx
@@ -1,7 +1,19 @@
-import "./Projects.css"
+import "./Projects.css";
 
-function Projects (){
-    return <h1>Projects</h1>
+import { projects } from "../../utils/projects";
+import ProjectCard from "../ProjectCard/ProjectCard";
+
+function Projects() {
+  const projectCards = projects.map((project) => (
+    <ProjectCard key={project.id} projectInfo={project} />
+  ));
+
+  return (
+    <div className="projects-page">
+      <h1 className="projects-page__title">Projects</h1>
+      <div className="projects-card__container">{projectCards}</div>
+    </div>
+  );
 }
 
 export default Projects;
